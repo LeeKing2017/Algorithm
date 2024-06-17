b4c74869fe422b8417c8053f502e887093cbaf96
LeeKing2017 <thrusum123@gmail.com>
2024-06-17 17:37:32 +0900

    완전탐색 카펫

---
+++ src/main/java/solution/Carpet.java
@@ -0,0 +1,28 @@
+package solution;
+
+import java.util.Arrays;
+
+public class Carpet {
+    public static void main(String[] args) {
+        int[] solution = solution(10, 2);
+
+        System.out.println("solution = " + Arrays.toString(solution));
+    }
+    public static int[] solution(int brown, int yellow) {
+        int[] answer = {};
+
+        int totalTiles = brown + yellow;
+
+        for (int i = totalTiles; i >= 3; i--) {
+            int width = i - 2;
+            int height = totalTiles - width;
+
+            if (height * width == totalTiles) {
+                answer = new int[]{width, height};
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
