4ae4c37648bf94bde273412c4f0568e261519d95
LeeKing2017 <thrusum123@gmail.com>
2024-06-27 04:50:40 +0900

    프로그래머스 - 기지국 설치

---
+++ src/main/java/solution/programmers/StationInstall.java
@@ -0,0 +1,29 @@
+package solution.programmers;
+
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/12979
+ * 기지국 설치
+ */
+public class StationInstall {
+    public static void main(String[] args) {
+        int solution = solution(11, new int[]{4, 11}, 1);
+        System.out.println("solution = " + solution);
+    }
+    public static int solution(int n, int[] stations, int w) {
+        int answer = 0;
+        int location = 1;
+        int idx = 0;
+
+        while (location <= n) {
+            if (idx <= stations.length - 1 && location >= stations[idx] - w) {
+                location = stations[idx] + w + 1;
+                idx++;
+            } else {
+                location += w * 2 + 1;
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
