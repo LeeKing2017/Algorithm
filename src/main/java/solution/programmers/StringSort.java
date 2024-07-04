7760adc6afa1898a569ceeaceb5d9ba8c9c5eb6e
LeeKing2017 <thrusum123@gmail.com>
2024-07-05 00:39:37 +0900

    프로그래머스 문자열 정렬

---
+++ src/main/java/solution/programmers/StringSort.java
@@ -0,0 +1,22 @@
+package solution.programmers;
+
+import java.util.*;
+
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/12915
+ * 문자열 내 마음대로 정렬하기
+ */
+public class StringSort {
+    public static void main(String[] args) {
+        String[] solution = solution(new String[]{"abce", "abcd", "cdx"}, 2);
+        System.out.println("solution = " + Arrays.toString(solution));
+    }
+
+    public static String[] solution(String[] strings, int n) {
+        Arrays.sort(strings, (a, b) -> a.charAt(n) == b.charAt(n)
+                ? a.compareTo(b)
+                : Character.compare(a.charAt(n), b.charAt(n)));
+
+        return strings;
+    }
+}
