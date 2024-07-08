41ca9cf1dee42fb4a93b7b2155933963b94a8920
LeeKing2017 <thrusum123@gmail.com>
2024-07-08 23:52:46 +0900

    프로그래머스 롤케이크 자르기

---
+++ src/main/java/solution/programmers/CutCake.java
@@ -0,0 +1,37 @@
+package solution.programmers;
+import java.util.*;
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/132265
+ * 롤케이크 자르기
+ */
+public class CutCake {
+
+    static class Solution {
+        public int solution(int[] topping) {
+            int answer = 0;
+            HashSet<Integer> chul = new HashSet<>();
+            HashMap<Integer, Integer> young = new HashMap<>();
+            for(int a : topping) {
+                young.put(a, young.getOrDefault(a, 0) + 1);
+            }
+
+            int toppingCnt = young.size();
+
+            for(int b : topping) {
+                if (young.get(b) == 1) {
+                    young.remove(b);
+                } else {
+                    young.put(b, young.get(b) - 1);
+                }
+
+                chul.add(b);
+
+                if (young.size() == chul.size()) {
+                    answer++;
+                }
+            }
+
+            return answer;
+        }
+    }
+}
