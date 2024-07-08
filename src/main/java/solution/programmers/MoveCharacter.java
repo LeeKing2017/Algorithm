1c8d57d11ab57017c52962e1cc3b2a94f539c333
LeeKing2017 <thrusum123@gmail.com>
2024-07-09 00:57:44 +0900

    프로그래머스 캐릭터의 좌표

---
+++ src/main/java/solution/programmers/MoveCharacter.java
@@ -0,0 +1,34 @@
+package solution.programmers;
+import java.util.*;
+
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120861
+ * 캐릭터의 좌표
+ */
+public class MoveCharacter {
+    static class Solution {
+
+        public int[] solution(String[] keyinput, int[] board) {
+            int[] answer = new int[]{0, 0};
+            HashMap<String, Integer[]> key = new HashMap<>();
+            key.put("right", new Integer[]{1, 0});
+            key.put("left", new Integer[]{-1, 0});
+            key.put("up", new Integer[]{0, 1});
+            key.put("down", new Integer[]{0, -1});
+
+            for(String input : keyinput) {
+                int nx = answer[0] + key.get(input)[0];
+                int ny = answer[1] + key.get(input)[1];
+
+                if (Math.abs(nx) > board[0] / 2 || Math.abs(ny) > board[1] / 2) {
+                    continue;
+                }
+
+                answer[0] = nx;
+                answer[1] = ny;
+            }
+
+            return answer;
+        }
+    }
+}
