debe7bc0735e717d88bec9e352c845d33f3c160b
LeeKing2017 <thrusum123@gmail.com>
2024-07-09 19:02:17 +0900

    프로그래머스 점프와 순간 이동

---
+++ src/main/java/solution/programmers/JumpAndMove.java
@@ -0,0 +1,22 @@
+package solution.programmers;
+
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/12980
+ * 점프와 순간 이동
+ */
+public class JumpAndMove {
+    public static class Solution {
+        public int solution(int n) {
+            int ans = n % 2 == 0 ? 0 : 1;
+
+            while (n != 1) {
+                n /= 2;
+
+                if (n % 2 == 1) {
+                    ans++;
+                }
+            }
+            return ans;
+        }
+    }
+}
