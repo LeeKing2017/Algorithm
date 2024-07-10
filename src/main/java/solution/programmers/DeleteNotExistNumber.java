6c731c2091c5cd1d55dac44bce4b9eadc765d4ca
LeeKing2017 <thrusum123@gmail.com>
2024-07-10 23:55:01 +0900

    프로그래머스 없는 숫자 더하기

---
+++ src/main/java/solution/programmers/DeleteNotExistNumber.java
@@ -0,0 +1,27 @@
+package solution.programmers;
+
+import java.util.*;
+import java.util.stream.*;
+
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/86051
+ * 없는 숫자 더하기
+ */
+public class DeleteNotExistNumber {
+    public static void main(String[] args) {
+        int solution = solution(new int[]{1, 2, 3, 4, 6, 7, 8, 0});
+        System.out.println("solution = " + solution);
+    }
+
+    public static int solution(int[] numbers) {
+        List<Integer> numList = IntStream.range(0, 10)
+                .boxed()
+                .collect(Collectors.toList());
+
+        for (int num : numbers) {
+            numList.remove(Integer.valueOf(num));
+        }
+
+        return numList.stream().mapToInt(Integer::intValue).sum();
+    }
+}
