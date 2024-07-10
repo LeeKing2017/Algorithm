d722bbe224987f633500ad7b2149fc04007e19da
LeeKing2017 <thrusum123@gmail.com>
2024-07-11 02:54:46 +0900

    프로그래머스 쿼드압축 후 개수 세기

---
+++ src/main/java/solution/programmers/QuadCompress.java
@@ -0,0 +1,43 @@
+package solution.programmers;
+
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/68936
+ * 쿼드압축 후 개수 세기
+ */
+public class QuadCompress {
+    private static int[][] Arr;
+    private static int[] answer;
+
+    public int[] solution(int[][] arr) {
+        Arr = arr;
+        answer = new int[2];
+
+        dp(0, 0, Arr.length);
+        return answer;
+    }
+
+    private static void dp(int x, int y, int size) {
+        if (check(x, y, size)) {
+            answer[Arr[x][y]]++;
+            return;
+        }
+
+        int nextSize = size / 2;
+        dp(x, y, nextSize);
+        dp(x + nextSize, y, nextSize);
+        dp(x, y + nextSize, nextSize);
+        dp(x + nextSize, y + nextSize, nextSize);
+    }
+
+    private static boolean check(int x, int y, int size) {
+        for (int i = x; i < x + size; i++) {
+            for (int j = y; j < y + size; j++) {
+                if (Arr[x][y] != Arr[i][j]) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
