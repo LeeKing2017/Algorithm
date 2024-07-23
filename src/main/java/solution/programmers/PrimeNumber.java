621b9eeb16a25aa0c9ee38ef133b71aa9a03718e
LeeKing2017 <thrusum123@gmail.com>
2024-07-24 02:05:13 +0900

    프로그래머스 k진수에서 소수 개수 구하기

---
+++ src/main/java/solution/programmers/PrimeNumber.java
@@ -0,0 +1,39 @@
+package solution.programmers;
+
+import java.util.Arrays;
+
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/92335?language=java
+ * k진수에서 소수 개수 구하기
+ */
+public class PrimeNumber {
+    public static void main(String[] args) {
+        solution(110011, 10);
+    }
+
+    public static int solution(int n, int k) {
+        int answer = 0;
+        String[] nums = Integer.toString(n, k).split("0");
+
+        for (String num : nums) {
+            if (!num.isEmpty() && isPrimeNum(Long.parseLong(num))) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+
+    public static boolean isPrimeNum(long num) {
+        if (num == 1) return false;
+        long maxI = (long) Math.sqrt(num) + 1;
+
+        for (int i = 2; i < maxI; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
