aede99cd2768e55ccd1dcdc523953cc0d64e6a16
LeeKing2017 <thrusum123@gmail.com>
2024-07-25 05:00:48 +0900

    프로그래머스 숫자 게임

---
+++ src/main/java/solution/programmers/NumCompete.java
@@ -0,0 +1,28 @@
+package solution.programmers;
+
+import java.util.Arrays;
+
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/12987?language=java
+ * 숫자 게임
+ */
+public class NumCompete {
+    public static void main(String[] args) {
+        int solution = solution(new int[]{5, 1, 3, 7}, new int[]{2, 2, 6, 8});
+        System.out.println("solution = " + solution);
+    }
+    public static int solution(int[] A, int[] B) {
+        int answer = 0;
+        int idx = B.length - 1;
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        for (int i = A.length - 1; i >= 0; i--) {
+            if (B[idx] > A[i]) {
+                answer++;
+                idx--;
+            }
+        }
+        return answer;
+    }
+}
