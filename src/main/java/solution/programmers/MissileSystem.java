e0b1d7ccb1137ffc18be1cccb022a53e6010b877
LeeKing2017 <thrusum123@gmail.com>
2024-07-30 06:10:34 +0900

    프로그래머스 미사일 요격

---
+++ src/main/java/solution/programmers/MissileSystem.java
@@ -0,0 +1,29 @@
+package solution.programmers;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181188
+ * 요격 시스템
+ */
+public class MissileSystem {
+    public static void main(String[] args) {
+        int solution = solution(new int[][]{{4, 5}, {4, 8}, {10, 14}, {11, 13}, {5, 12}, {3, 7}, {1, 4}});
+        System.out.println("solution = " + solution);
+    }
+
+    public static int solution(int[][] targets) {
+        int answer = 0;
+        Arrays.sort(targets, Comparator.comparingInt(o -> o[1]));
+
+        int before = 0;
+        for (int[] target : targets) {
+            if (before <= target[0]) {
+                before = target[1];
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
