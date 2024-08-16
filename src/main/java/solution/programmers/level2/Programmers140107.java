97999895ef5f65a287f990874a7f2af83796712a
LeeKing2017 <thrusum123@gmail.com>
2024-08-17 00:32:14 +0900

    프로그래머스 점 찍기

---
+++ src/main/java/solution/programmers/level2/Programmers140107.java
@@ -0,0 +1,23 @@
+package solution.programmers.level2;
+
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/140107
+ * 점 찍기
+ */
+public class Programmers140107 {
+    public static void main(String[] args) {
+        long solution = solution(10, 2);
+        System.out.println("solution = " + solution);
+    }
+
+    public static long solution(long k, long d) {
+        long answer = 0;
+
+        for(long x = 0; x <= d; x += k){
+            long maxY = (long) Math.sqrt(d*d - x*x);
+            answer += maxY / k + 1;
+        }
+
+        return answer;
+    }
+}
